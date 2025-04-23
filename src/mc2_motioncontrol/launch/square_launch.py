668cf9492786661cff10697b6823269c97684ca6
aldrick-t <taldrick@gmail.com>
2025-04-23 15:11:50 -0600

    Major: new square control system. Testing.

---
+++ src/mc2_motioncontrol/launch/square_launch.py
@@ -0,0 +1,42 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Get package directory
+    package_directory = get_package_share_directory('mc2_motioncontrol')
+
+        # Load the parameters from the YAML file
+    config = os.path.join(
+        package_directory,
+        'config',
+        'square_params.yaml'
+    )
+
+    path_generator = Node(
+            package='mc2_motioncontrol',
+            executable='path_generator',
+            name='path_generator',
+            parameters=[{'use_sim_time': True}, config],
+        )
+
+    square_control = Node(
+            package='mc2_motioncontrol',
+            executable='square_control',
+            name='square_control',
+            parameters=[{'use_sim_time': True}, config]
+        )
+    
+    odometry_node = Node(
+            package='mc2_motioncontrol',
+            executable='odometry_node',
+            name='odometry_node',
+            parameters=[{'use_sim_time': True}]
+        )
+
+    return LaunchDescription([
+        square_control,
+        path_generator,
+        odometry_node,
+    ])
--- src/mc2_motioncontrol/mc2_motioncontrol/square_control.py
+++ src/mc2_motioncontrol/mc2_motioncontrol/square_control.py
@@ -1,34 +1,42 @@
 '''
-Square Path Controller Node with PID Control
-The robot moves in a square path, turning at each corner.
-The robot stops after completing the square path.
-Goal points (each corner) are set via params.
+User Defined Path Controller Node with PID Control
+The robot moves in the set path, calculating pose and velocity.
+The robot stops after completing the path.
+Goal points are set via params.
 '''
 
 import rclpy
 from rclpy.node import Node
+from rcl_interfaces.msg import SetParametersResult
 from geometry_msgs.msg import Pose2D
+import rclpy.parameter
+from std_msgs.msg import Bool, Empty
 from geometry_msgs.msg import Twist
 import numpy as np
 import signal # To handle Ctrl+C
 import sys # To exit the program
 
-class squareControl(Node):
+class pathControl(Node):
     def __init__(self):
         super().__init__('square_control')
         self.wait_for_ros_time()
 
         self.cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.pose_sub = self.create_subscription(Twist, 'pose', self.pose_callback, 10)
+        self.next_goal_pub = self.create_publisher(Empty, 'next_goal', 10)
+        self.pose_sub = self.create_subscription(Twist, 'pose', self.pose_cb, 10)
+        self.goal_sub = self.create_subscription(Pose2D, 'goal', self.goal_cb, 10)
         
         # Handle shutdown gracefully
         signal.signal(signal.SIGINT, self.shutdown_function) # When Ctrl+C is pressed, call self.shutdown_function
 
         # Declare parameters
-        self.square_side = self.declare_parameter('square_side', 2.0).get_parameter_value().double_value
         self.robust_margin = self.declare_parameter('robust_margin', 0.9).get_parameter_value().double_value
+        self.goal_threshold = self.declare_parameter('goal_threshold', 0.05).get_parameter_value().double_value
+
+        self.add_on_set_parameters_callback(self.parameter_callback)
 
         self.goal_received = False
+        self.next_goal_pub.publish(Empty()) # Publish empty message to notify next goal
         self.xg = 0.0 # Goal position x[m]
         self.yg = 0.0 # Goal position y[m]
 
@@ -40,14 +48,81 @@ class squareControl(Node):
         self.kp_v = self.declare_parameter('kp_v', 0.5).get_parameter_value().double_value # Linear velocity gain
         self.kp_w = self.declare_parameter('kp_w', 1.0).get_parameter_value().double_value # Angular velocity gain
 
-        # self.ed = 0.0 # Error in x[m]
-        # self.etheta = 0.0 # Error in y[m]
-        
         self.cmd_vel = Twist()
         timer_period = 0.05 
         self.create_timer(timer_period, self.main_timer_cb)
         self.get_logger().info("Node initialized!!")
 
+    def main_timer_cb(self):
+        ## This function is called every 0.05 seconds
+        if self.goal_received:
+
+            self.get_logger().info("Goal received")
+            self.get_logger().info(f"Moving to goal: x={self.xg:.2f}, y={self.yg:.2f}")
+
+            ed, etheta = self.get_errors(self.xr, self.yr, self.xg, self.yg, self.theta_r)
+
+            # Goal Threshold
+            if ed < self.goal_threshold: #Threshold value (tolerance) for goal reached in meters.
+                self.get_logger().info(f"Goal reached : x={self.xg:.2f}, y={self.yg:.2f}")
+                self.get_logger().info(f"Current pose : x={self.xr:.2f}, y={self.yr:.2f}")
+                self.get_logger().info(f"Current theta: {self.theta_r:.2f}")
+                self.get_logger().info(f"Within thresh: {ed:.2f} m")
+                self.goal_received = False
+                self.get_logger().debug(f"Goal received: {self.goal_received}")
+                self.next_goal_pub.publish(Empty()) # Publish empty message to notify next goal
+                self.get_logger().debug("Requested next goal")
+                self.cmd_vel.linear.x = 0.0
+                self.cmd_vel.angular.z = 0.0
+            else:
+                if etheta < 0.01:
+                    self.cmd_vel.angular.z = 0.0
+                    self.cmd_vel.linear.x = self.kp_v * ed
+                    self.get_logger().debug(f"Linear velocity: {self.cmd_vel.linear.x:.2f} m/s")
+                    if self.cmd_vel.linear.x > 0.6:
+                        self.get_logger().warn(f"Linear velocity above safe limit: {self.cmd_vel.linear.x:.2f} m/s")
+                    self.cmd_vel_pub.publish(self.cmd_vel)
+                else:
+                    self.cmd_vel.linear.x = 0.0
+                    self.cmd_vel.angular.z = self.kp_w * etheta
+                    self.get_logger().debug(f"Angular velocity: {self.cmd_vel.angular.z:.2f} rad/s")
+                    if self.cmd_vel.angular.z > 1.5:
+                        self.get_logger().warn(f"Angular velocity above safe limit: {self.cmd_vel.angular.z:.2f} rad/s")
+                    self.cmd_vel_pub.publish(self.cmd_vel)
+
+
+                # self.cmd_vel.linear.x = self.kp_v * ed
+
+                # self.get_logger().debug(f"Linear velocity: {self.cmd_vel.linear.x:.2f} m/s")
+                # if self.cmd_vel.linear.x > 0.6:
+                #     self.get_logger().warn(f"Linear velocity above safe limit: {self.cmd_vel.linear.x:.2f} m/s")
+                
+                # self.cmd_vel.angular.z = self.kp_w * etheta
+
+                # self.get_logger().debug(f"Angular velocity: {self.cmd_vel.angular.z:.2f} rad/s")
+                # if self.cmd_vel.angular.z > 1.5:
+                #     self.get_logger().warn(f"Angular velocity above safe limit: {self.cmd_vel.angular.z:.2f} rad/s")
+
+            self.cmd_vel_pub.publish(self.cmd_vel)
+        else: 
+            self.get_logger().info("Waiting for goal")
+            self.get_logger().debug(f"Goal received: {self.goal_received}")
+
+            
+
+    def get_errors(self, xr, yr, xg, yg, theta_r):
+        ## This function computes the errors in x and y
+        # Compute the distance to the goal
+        ed = np.sqrt((xg - xr)**2 + (yg - yr)**2)
+        # Compute the angle to the goal
+        thetag = np.arctan2(yg - yr, xg - xr)
+        etheta = thetag - theta_r
+        # Normalize the angle to be between -pi and pi
+        etheta = np.arctan2(np.sin(etheta), np.cos(etheta))
+        # Debug prints
+        self.get_logger().info(f"Distance to goal: {ed:.2f} m")
+        self.get_logger().info(f"Angle to goal: {etheta:.2f} rad")
+        return ed, etheta
 
     def pose_cb(self, pose): 
         ## This function receives the /pose from the odometry_node
@@ -71,6 +146,22 @@ class squareControl(Node):
             rclpy.spin_once(self, timeout_sec=0.1)
         self.get_logger().info('ROS time is active!')
 
+    def parameter_callback(self, params):
+        for param in params:
+            if param.name == 'robust_margin' and param.type_ == param.Type.DOUBLE:
+                self.robust_margin = param.value
+                self.get_logger().info(f"Updated robust_margin: {self.robust_margin}")
+            elif param.name == 'goal_threshold' and param.type_ == param.Type.DOUBLE:
+                self.goal_threshold = param.value
+                self.get_logger().info(f"Updated goal_threshold: {self.goal_threshold}")
+            elif param.name == 'kp_v' and param.type_ == param.Type.DOUBLE:
+                self.kp_v = param.value
+                self.get_logger().info(f"Updated kp_v: {self.kp_v}")
+            elif param.name == 'kp_w' and param.type_ == param.Type.DOUBLE:
+                self.kp_w = param.value
+                self.get_logger().info(f"Updated kp_w: {self.kp_w}")
+        return SetParametersResult(successful=True)
+
     def shutdown_function(self, signum, frame):
         # Handle shutdown gracefully
         # This function will be called when Ctrl+C is pressed
@@ -83,7 +174,7 @@ class squareControl(Node):
     
 def main(args=None):
     rclpy.init(args=args)
-    node = squareControl()
+    node = pathControl()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
