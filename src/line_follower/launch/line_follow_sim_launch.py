a21f904becc338ab848e3216b69c042faed0af64
aldrick-t <taldrick@gmail.com>
2025-05-21 16:23:31 -0600

    Major Complete: Complete integration of line follower system. Untracked dev. Revamped node interconnection and package structure. Full dev, traffic light detection revamp pending.

---
+++ src/line_follower/launch/line_follow_sim_launch.py
@@ -0,0 +1,85 @@
+'''
+line_follow_sim_launch.py
+
+Launch file for FULL line_follower package in SIMULATION mode.
+Nodes: line_recogni, cam_preprocess, visual_monitor, line_cmd, robot_ctrl, trafficlight_recogni
+Parameters: cv_params.yaml, control_params.yaml
+'''
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    # Get package directory
+    package_directory = get_package_share_directory('line_follower')
+    
+    # Load the parameters from the YAML file
+    config_cv = os.path.join(
+        package_directory,
+        'config',
+        'cv_params.yaml'
+    )
+    
+    config_ctrl = os.path.join(
+        package_directory,
+        'config',
+        'control_params.yaml'
+    )
+    
+    cam_preprocess = Node(
+            package='line_follower',
+            executable='cam_preprocess',
+            name='cam_preprocess',
+            parameters=[{'use_sim_time': True,
+                         'camera_topic': 'camera',
+                         }, config_cv]
+        )
+    
+    line_recogni = Node(
+            package='line_follower',
+            executable='line_recogni',
+            name='line_recogni',
+            parameters=[{'use_sim_time': True,}, config_cv]
+        )
+    
+    visual_monitor = Node(
+            package='line_follower',
+            executable='visual_monitor',
+            name='visual_monitor',
+            parameters=[{'use_sim_time': True,
+                         'camera_topic': 'camera',}]
+        )
+    
+    line_cmd = Node(
+            package='line_follower',
+            executable='line_cmd',
+            name='line_cmd',
+            parameters=[{'use_sim_time': True}]
+        )
+    
+    robot_ctrl = Node(
+            package='line_follower',
+            executable='robot_ctrl',
+            name='robot_ctrl',
+            parameters=[{'use_sim_time': True}, config_ctrl]
+        )
+    
+    trafficlight_recogni = Node(
+            package='line_follower',
+            executable='trafficlight_recogni_legacy',
+            name='trafficlight_recogni_legacy',
+            parameters=[{'use_sim_time': True,
+                         'camera_topic': 'camera',
+                         }, config_cv]
+        )
+    
+    return LaunchDescription([
+        cam_preprocess,
+        line_recogni,
+        visual_monitor,
+        line_cmd,
+        robot_ctrl,
+        trafficlight_recogni,
+    ])
