c8943caecb783864591e60ae20a11c9f85a712aa
aldrick-t <taldrick@gmail.com>
2025-05-22 00:07:13 -0600

    Docs: Package docs improved.

--- src/line_follower/setup.py
+++ src/line_follower/setup.py
@@ -5,7 +5,7 @@ package_name = 'line_follower'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -18,8 +18,9 @@ setup(
     zip_safe=True,
     maintainer='atad',
     maintainer_email='taldrick@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='A package for line following robot, built for ROS2 on a MCR2 Puzzlebot.',
+    long_description='A package for line following robot, built for ROS2 on a MCR2 Puzzlebot. This package includes various nodes for camera preprocessing, line recognition, robot control, and traffic light recognition.',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
