b51c0e0a5fcc67c6ef7e70e826dfbbfdd5fd7330
Diego Quezada <a01710024@tec.mx>
2025-05-23 22:12:40 -0600

    fix logger

--- src/line_follower/line_follower/line_cmd.py
+++ src/line_follower/line_follower/line_cmd.py
@@ -113,12 +113,14 @@ class LineCmd(Node):
             cmd_msg = 1.0 - cmd_msg
             cmd_msg *= -1.0
         elif cmd_msg < -1.1:
-            cmd_msg += int(cmd_msg)
             self.get_logger().debug(f"AAAAAAAAAAAAAAAAAAAA: {cmd_msg}", throttle_duration_sec=1.0)
+            cmd_msg -= int(cmd_msg)
+            self.get_logger().debug(f"BBBBBBBBBBBBBBBBBBBBB: {cmd_msg}", throttle_duration_sec=1.0)
             cmd_msg = -1.0 + cmd_msg
+            self.get_logger().debug(f"CCCCCCCCCCCCCCCCCCCCCCCCC: {cmd_msg}", throttle_duration_sec=1.0)
             cmd_msg *= -1.0
         #self.get_logger().debug("SEXOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO", throttle_duration_sec=1.0)
-        cmd_msg = Float32()
+        cmd_msg.astype(Float32)
         self.line_cmd_pub.publish(cmd_msg)
         
         
