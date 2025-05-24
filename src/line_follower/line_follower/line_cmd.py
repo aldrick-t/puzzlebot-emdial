39e652c8b2e46c98e06c0cb0a0916deb4ac8fd93
Diego Quezada <a01710024@tec.mx>
2025-05-23 22:35:53 -0600

    fixing down

--- src/line_follower/line_follower/line_cmd.py
+++ src/line_follower/line_follower/line_cmd.py
@@ -113,12 +113,12 @@ class LineCmd(Node):
             cmd_msg = 1.0 - cmd_msg
             cmd_msg *= -1.0
         elif cmd_msg < -1.1:
-            self.get_logger().debug(f"AAAAAAAAAAAAAAAAAAAA: {cmd_msg}", throttle_duration_sec=1.0)
+            #self.get_logger().debug(f"AAAAAAAAAAAAAAAAAAAA: {cmd_msg}", throttle_duration_sec=1.0)
             cmd_msg += int(cmd_msg) * -1.0
-            self.get_logger().debug(f"BBBBBBBBBBBBBBBBBBBBB: {cmd_msg}", throttle_duration_sec=1.0)
+            #self.get_logger().debug(f"BBBBBBBBBBBBBBBBBBBBB: {cmd_msg}", throttle_duration_sec=1.0)
             cmd_msg = 1.0 + cmd_msg
-            self.get_logger().debug(f"CCCCCCCCCCCCCCCCCCCCCCCCC: {cmd_msg}", throttle_duration_sec=1.0)
-            cmd_msg *= -1.0
+            #self.get_logger().debug(f"CCCCCCCCCCCCCCCCCCCCCCCCC: {cmd_msg}", throttle_duration_sec=1.0)
+            #cmd_msg *= -1.0
         #self.get_logger().debug("SEXOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO", throttle_duration_sec=1.0)
         cmd_msg_t = Float32()
         cmd_msg_t.data = cmd_msg
